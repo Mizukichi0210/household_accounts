52ba4f4fda6532b6627e034100fd5115683d412b
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-04-07 21:59:04 +0900

    ヘルプ追加

--- household_account.js
+++ household_account.js
@@ -18,3 +18,11 @@ var controller = Botkit.slackbot({
 controller.spawn({
     token : process.env.token
 }).startRTM();
+
+controller.hears(["(help),(ヘルプ)"], ['direct_message'], (bot,message) =>{
+	bot.reply(message,">目的\n支出金額\n>金額確認\n目的\n>ユーザ登録\n名前\n>help or ヘルプ");
+});
+
+controller.hears(["(.*)"], ['direct_message'], (bot,message) =>{
+	bot.reply(message,"");
+});
