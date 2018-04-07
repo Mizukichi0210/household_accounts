fa7991399980c17c2821d8566b6b3fe4311c2f68
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-04-07 21:56:29 +0900

    起動準備コードアップロード

--- household_account.js
+++ household_account.js
@@ -1,1 +1,20 @@
-console.log("abc");
+var Botkit = require("botkit");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'test',
+  password : 'test',
+  database : 'household_account',
+  timezone : 'jst'
+});
+
+var controller = Botkit.slackbot({
+  debug: false       
+  // include "log: false" to disable logging
+  // or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+})
+
+controller.spawn({
+    token : process.env.token
+}).startRTM();
