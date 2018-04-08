f8eaff36b9b36eb1225a386f1ffecdf4c83e120e
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-04-08 18:14:40 +0900

    登録した支出の確認をするコード追加

--- household_account.js
+++ household_account.js
@@ -17,25 +17,6 @@ var controller = Botkit.slackbot({
   // or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 })
 
-/*
-function check_purpose(callback,purpose){
-	var checkSql = "select count(*) as cnt from purpose where purpose = ?";
-	con.query(checkSql,[purpose],function(err,result,fields){
-		return new Promise(function(resolve,reject){
-			resolve(result[0].cnt);
-		},1000);
-	});
-}*/
-/*
-checkPurpose = function(callback,purpose){
-	console.log(purpose);
-	var checkSql = "select count(*) as cnt from purpose where purpose = ?";
-	con.query(checkSql,[purpose],function(err,result,fields){
-		console.log(result[0].cnt);
-		return callback(result[0].cnt);
-	});
-}
-*/
 controller.spawn({
     token : process.env.token
 }).startRTM();
@@ -43,7 +24,7 @@ controller.spawn({
 // ↓　コマンドの種類表示
 
 controller.hears(["(help)","(ヘルプ)"], ['direct_message'], (bot,message) =>{
-	bot.reply(message,">支出記録 or 支出登録\n支出金額\n目的\n>金額確認\n目的\n>ユーザ登録\n名前\n>help or ヘルプ");
+	bot.reply(message,">支出記録 or 支出登録 or 登録 or 記録\n支出金額\n目的\n>支出確認 or 確認\n目的\n>ユーザ登録\n名前\n>目的 or 目的確認 or 目的一覧\n>help or ヘルプ");
 });
 
 // ↓　ユーザ登録の処理
@@ -85,9 +66,10 @@ controller.hears(["(ユーザ登録)"], ['direct_message'], (bot,message) =>{
 
 //　↓　支出登録の処理
 
-controller.hears(["(支出登録)","(支出記録)"], ['direct_message'], (bot,message) =>{
+controller.hears(["(支出登録)","(支出記録)","(登録)","(記録)"], ['direct_message'], (bot,message) =>{
 	var expenditure = message.text.split("\n")[1];
 	var purpose = message.text.split("\n")[2];
+	var slack_id;
 	
 	// ↓ 金額と目的が入力されているかのチェック
 	
@@ -103,42 +85,115 @@ controller.hears(["(支出登録)","(支出記録)"], ['direct_message'], (bot,m
 					};
 				}
 				controller.storage.users.save(user_info, function (err, id) {
+					slack_id = user_info.id;
 				});
+			});
 				
 				// ↓ usersからuseridを取得
 				
-				var confirm_users = "select * from users where slack_id = ?";
-				con.query(confirm_users,[user_info.id],function(err,res,fields){
-					
-					// ↓ purposeテーブルに登録されているかチェック
-					
-					var checkPurpose = "select count(*) as cnt from purpose where purpose = ?";
-					con.query(checkPurpose,[purpose],function(err,row){
-						if(row[0].cnt == 0) bot.reply(message,"入力した目的がテーブルに登録されてません!");
-						else{
-							
-							// ↓ purposeテーブルからpurpose_idを取得
-							
-							var getPurposeId = "select * from purpose where purpose = ?";
-							con.query(getPurposeId,[purpose],function(err,result){
-							
-								// ↓ expenditureテーブルへinsert
+			var confirm_users = "select *,count(*) as countUsers from users where slack_id = ?";
+			con.query(confirm_users,[slack_id],function(err,res,fields){
 				
-								var registerExpenditure = "insert into expenditure (expenditure,purpose_id,user_id,date) values (?,?,?,?)";
-								con.query(registerExpenditure,[expenditure,result[0].id,res[0].id,now.toFormat('YYYY-MM-DD')],function(err,row){
-									bot.reply(message,"登録完了しました!");
-								});
-							});
-						}
+				if(res[0].countUsers == 0){
+					bot.reply(message, "ユーザデータが登録されていません");
+					return;
+				}
+
+				// ↓ purposeテーブルからpurpose_idを取得
+							
+				var getPurposeId = "select *,count(*) as cnt from purpose where purpose = ?";
+				con.query(getPurposeId,[purpose],function(err,result){
+					
+					if(result[0].cnt == 0){
+						bot.reply(message,"目的がテーブルに登録されてません!");
+						return;
+					}
+							
+					// ↓ expenditureテーブルへinsert
+				
+					var registerExpenditure = "insert into expenditure (expenditure,purpose_id,user_id,date) values (?,?,?,?)";
+					con.query(registerExpenditure,[expenditure,result[0].id,res[0].id,now.toFormat('YYYY-MM-DD')],function(err,row){
+						bot.reply(message,"登録完了しました!");
 					});
 				});
 			});
-		}else bot.reply(message,"2行目には金額を入力!");
+		}
 	}
+});
+
+// ↓　登録した支出の確認。目的はあってもなくてもok
+
+controller.hears(["(支出確認)","(確認)"],['direct_message'],(bot,message)=>{
+	var purpose = message.text.split("\n")[1];
+	var slack_id;
+	
+	controller.storage.users.get(message.user, function (err, user_info) {
+		if (!user_info) {
+			user_info = {
+				id: message.user,
+			};
+		}
+		controller.storage.users.save(user_info, function (err, id) {
+			slack_id = user_info.id;
+		});
+	});
+	
+	var getUsersId = "select * from users where slack_id = ?";
+	con.query(getUsersId,[slack_id],function(err,result,fields){
+			
+		// ↓ 目的指定なしの使用金額確認
+			
+		if(purpose == undefined){
+			let getExpenditure = "select purpose_id,sum(expenditure) as exp from expenditure where user_id = ? group by purpose_id";
+			con.query(getExpenditure,[result[0].id],function(err,rows,fields){
+				for(let i in rows){
+					let getPurpose = "select * from purpose where id = ?";
+					con.query(getPurpose,[rows[i].purpose_id],function(err,res,fields){
+						bot.reply(message,res[0].purpose + " : *" + rows[i].exp + "* 円\n");
+					});
+				}
+			});
+		}
+			
+		// ↓ 目的指定ありの使用金額確認
+			
+		else{
+			let getPurposeId = "select *,count(*) as cntPurpose from purpose where purpose = ?";
+			con.query(getPurposeId,[purpose],function(err,rows,fields){
+				
+				// ↓ 目的がテーブルに登録されていた場合
+					
+				if(rows[0].cntPurpose != 0){
+					let getExpenditure = "select *,count(*) as cnt,sum(expenditure) as exp from expenditure where user_id = ? and purpose_id = ?";
+					con.query(getExpenditure,[result[0].id,rows[0].id],function(err,res,fields){
+						if(res[0].cnt != 0) bot.reply(message,"*" + res[0].exp + "* 円");
+						else bot.reply(message,"*0* 円");
+					});
+				}
+				
+				// ↓　目的がテーブルに登録されていなかった場合
+				
+				else{
+					bot.reply(message,"*0* 円");
+				}
+			});
+		}
+	});
+});
+
+// ↓　目的一覧表示
+
+controller.hears(["(目的)","(目的確認)","(目的一覧)"],['direct_message'],(bot,message) =>{
+	let getPurpose = "select * from purpose";
+	con.query(getPurpose,function(err,result,fields){
+		for(let i in result){
+			bot.reply(message,result[i].purpose);
+		}
+	});
 });
 
 // ↓　その他のメッセージが入力された場合の処理
 
 controller.hears(["(.*)"], ['direct_message'], (bot,message) =>{
-	bot.reply(message,"help or ヘルプ\n参照");
+	bot.reply(message,"help or ヘルプ\nを参照してください");
 });
