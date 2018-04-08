4204809598f94f90faf83aa4d56110c4e9a4c5b3
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-04-08 16:26:05 +0900

    数値チェック、目的テーブルに登録されているかのチェック追加

--- household_account.js
+++ household_account.js
@@ -18,21 +18,32 @@ var controller = Botkit.slackbot({
 })
 
 /*
-function insert_users(slack_id){
-	var insertSql = "";
-	con.query(confirm_users,[user_info.id],function(err,res,fields){
-		if(res[0].id == undefined) return bot.reply()
+function check_purpose(callback,purpose){
+	var checkSql = "select count(*) as cnt from purpose where purpose = ?";
+	con.query(checkSql,[purpose],function(err,result,fields){
+		return new Promise(function(resolve,reject){
+			resolve(result[0].cnt);
+		},1000);
 	});
-});*/
-
+}*/
+/*
+checkPurpose = function(callback,purpose){
+	console.log(purpose);
+	var checkSql = "select count(*) as cnt from purpose where purpose = ?";
+	con.query(checkSql,[purpose],function(err,result,fields){
+		console.log(result[0].cnt);
+		return callback(result[0].cnt);
+	});
+}
+*/
 controller.spawn({
     token : process.env.token
 }).startRTM();
 
 // ↓　コマンドの種類表示
 
-controller.hears(["(help),(ヘルプ)"], ['direct_message'], (bot,message) =>{
-	bot.reply(message,">目的\n支出金額\n>金額確認\n目的\n>ユーザ登録\n名前\n>help or ヘルプ");
+controller.hears(["(help)","(ヘルプ)"], ['direct_message'], (bot,message) =>{
+	bot.reply(message,">支出記録 or 支出登録\n支出金額\n目的\n>金額確認\n目的\n>ユーザ登録\n名前\n>help or ヘルプ");
 });
 
 // ↓　ユーザ登録の処理
@@ -74,48 +85,55 @@ controller.hears(["(ユーザ登録)"], ['direct_message'], (bot,message) =>{
 
 //　↓　支出登録の処理
 
-controller.hears(["(支出登録)"], ['direct_message'], (bot,message) =>{
+controller.hears(["(支出登録)","(支出記録)"], ['direct_message'], (bot,message) =>{
 	var expenditure = message.text.split("\n")[1];
 	var purpose = message.text.split("\n")[2];
 	
 	// ↓ 金額と目的が入力されているかのチェック
 	
-	if(expenditure == undefined || purpose == undefined) bot.reply(message,">2行目:金額\n>3行目:目的");
+	if(expenditure == undefined || purpose == undefined) bot.reply(message,"2行目:*金額*\n3行目:*目的*");
 	else{
-		//　↓　user_info.idの取得
+		if(!isNaN(expenditure)){
+			//　↓　user_info.idの取得
 		
-		controller.storage.users.get(message.user, function (err, user_info) {
-			if (!user_info) {
-				user_info = {
-					id: message.user,
-				};
-			}	
-			controller.storage.users.save(user_info, function (err, id) {
-			});
-			// ↓ usersからuseridを取得
-			var confirm_users = "select * from users where slack_id = ?";
-			con.query(confirm_users,[user_info.id],function(err,res,fields){
-				if(res[0].id == undefined) return bot.reply(message,"*ユーザデータが登録されていません!*\nhelpを参照して!");
-				else{
+			controller.storage.users.get(message.user, function (err, user_info) {
+				if (!user_info) {
+					user_info = {
+						id: message.user,
+					};
+				}
+				controller.storage.users.save(user_info, function (err, id) {
+				});
+				
+				// ↓ usersからuseridを取得
+				
+				var confirm_users = "select * from users where slack_id = ?";
+				con.query(confirm_users,[user_info.id],function(err,res,fields){
 					
-					// ↓ purposeテーブルからpurpose_idを取得
-		
-					var getPurposeId = "select * from purpose where purpose = ?";
-					con.query(getPurposeId,[purpose],function(err,result){
-						if(result[0].id == undefined) return bot.reply(message,"入力した目的がテーブルに登録されてません!")
+					// ↓ purposeテーブルに登録されているかチェック
+					
+					var checkPurpose = "select count(*) as cnt from purpose where purpose = ?";
+					con.query(checkPurpose,[purpose],function(err,row){
+						if(row[0].cnt == 0) bot.reply(message,"入力した目的がテーブルに登録されてません!");
 						else{
 							
-						// ↓ expenditureテーブルへinsert
+							// ↓ purposeテーブルからpurpose_idを取得
+							
+							var getPurposeId = "select * from purpose where purpose = ?";
+							con.query(getPurposeId,[purpose],function(err,result){
+							
+								// ↓ expenditureテーブルへinsert
 				
-							var registerExpenditure = "insert into expenditure (expenditure,purpose_id,user_id,date) values (?,?,?,?)";
-							con.query(registerExpenditure,[expenditure,result[0].id,res[0].id,now.toFormat('YYYY-MM-DD')],function(err,row){
-								bot.reply(message,"登録完了しました!");
+								var registerExpenditure = "insert into expenditure (expenditure,purpose_id,user_id,date) values (?,?,?,?)";
+								con.query(registerExpenditure,[expenditure,result[0].id,res[0].id,now.toFormat('YYYY-MM-DD')],function(err,row){
+									bot.reply(message,"登録完了しました!");
+								});
 							});
 						}
 					});
-				}
+				});
 			});
-		});
+		}else bot.reply(message,"2行目には金額を入力!");
 	}
 });
 
